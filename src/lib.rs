37ccbac4c04f2593c8271791648254b5297dd8f0
JonathanHallstrom <lmj.hallstrom@gmail.com>
2023-04-06 15:54:34 +0200

    addition now working!

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,99 @@
-use likely_stable::{likely, unlikely};
 use num_bigint::BigUint;
 use std::ops::Add;
 
-use crate::util::add_tiny;
-
-pub mod util {
-    pub fn add_tiny(a: u64, b: u64) -> (u64, u64) {
-        let (sum, carry) = a.overflowing_add(b);
-        (sum, carry as u64)
-    }
-    pub fn add_small(a: u128, b: u128) -> (u128, u128) {
-        let (sum, carry) = a.overflowing_add(b);
-        (sum, carry as u128)
+#[derive(Debug, PartialEq, PartialOrd)]
+pub enum CHANGEME {
+    Small(u64),
+    Medium(u128),
+    Large(BigUint),
+}
+impl Default for CHANGEME {
+    fn default() -> Self {
+        CHANGEME::Small(0)
     }
 }
 
-pub enum CHANGEME {
-    Tiny(u64),
-    Small(u128),
-    Big(BigUint),
+impl From<u8> for CHANGEME {
+    fn from(value: u8) -> Self {
+        CHANGEME::Small(value as u64)
+    }
+}
+impl From<u16> for CHANGEME {
+    fn from(value: u16) -> Self {
+        CHANGEME::Small(value as u64)
+    }
+}
+impl From<u32> for CHANGEME {
+    fn from(value: u32) -> Self {
+        CHANGEME::Small(value as u64)
+    }
+}
+impl From<u64> for CHANGEME {
+    fn from(value: u64) -> Self {
+        CHANGEME::Small(value)
+    }
+}
+impl From<u128> for CHANGEME {
+    fn from(value: u128) -> Self {
+        CHANGEME::Medium(value)
+    }
+}
+impl From<BigUint> for CHANGEME {
+    fn from(value: BigUint) -> Self {
+        CHANGEME::Large(value)
+    }
+}
+impl From<&BigUint> for CHANGEME {
+    fn from(value: &BigUint) -> Self {
+        CHANGEME::Large(value.clone())
+    }
+}
+
+pub mod util {
+    use likely_stable::*;
+    use num_bigint::BigUint;
+
+    use crate::CHANGEME;
+
+    pub fn add_small(a: u64, b: u64) -> CHANGEME {
+        let (sum, carry) = a.overflowing_add(b);
+        if likely(!carry) {
+            CHANGEME::Small(sum)
+        } else {
+            CHANGEME::Medium(((carry as u128) << 64) + sum as u128)
+        }
+    }
+
+    pub fn add_medium(a: u128, b: u128) -> CHANGEME {
+        let (sum, carry) = a.overflowing_add(b);
+        if likely(!carry) {
+            CHANGEME::Medium(sum)
+        } else {
+            CHANGEME::Large((BigUint::from(carry as u64) << 128) + sum)
+        }
+    }
+
+    pub fn add_large_to_medium(a: &BigUint, b: u128) -> CHANGEME {
+        let mut res = BigUint::from(b);
+        res += a;
+        CHANGEME::Large(res)
+    }
+
+    pub fn add_large(a: &BigUint, b: &BigUint) -> CHANGEME {
+        CHANGEME::Large(a + b)
+    }
 }
 
 impl CHANGEME {
     fn compute_sum(&self, other: &CHANGEME) -> CHANGEME {
         use CHANGEME::*;
         match (self, other) {
-            (Tiny(a), Tiny(b)) => {
-                let (sum, carry) = add_tiny(*a, *b);
-                if likely(carry == 0) {
-                    Tiny(sum)
-                } else {
-                    Small((sum as u128) << 64 + carry as u128)
-                }
-            }
-            _ => Tiny(0),
+            (Small(a), Small(b)) => util::add_small(*a, *b),
+            (Small(a), Medium(b)) => util::add_medium(*a as u128, *b),
+            (Medium(a), Small(b)) => util::add_medium(*a, *b as u128),
+            (Medium(a), Medium(b)) => util::add_medium(*a, *b),
+            (Medium(a), Large(b)) => util::add_large_to_medium(b, *a),
+            _ => Small(0),
         }
     }
 }
@@ -42,5 +102,12 @@ impl Add<&CHANGEME> for &CHANGEME {
     type Output = CHANGEME;
     fn add(self, rhs: &CHANGEME) -> Self::Output {
         self.compute_sum(rhs)
+    }
+}
+
+impl Add<CHANGEME> for CHANGEME {
+    type Output = CHANGEME;
+    fn add(self, rhs: CHANGEME) -> Self::Output {
+        self.compute_sum(&rhs)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
-// use num_bigint::{BigInt, Sign};
-
-fn main() {
-
-}
+fn main() {}
 
 #[cfg(test)]
 mod tests {
     use arbitrary_int::*;
-    
+    use num_bigint::BigUint;
+
     #[test]
-    fn small_test() {
-        assert_eq!(util::add_tiny(1, 1), (2, 0));
+    fn test_small_add() {
+        assert_eq!(
+            CHANGEME::from(1_u32) + CHANGEME::from(2_u32),
+            CHANGEME::from(3_u32)
+        );
     }
 
     #[test]
-    fn big_test() {
-        assert_eq!(util::add_tiny(u64::MAX, u64::MAX), (u64::MAX - 1, 1));
+    fn test_medium_add() {
+        assert_eq!(
+            CHANGEME::from(u64::MAX) + CHANGEME::from(u64::MAX),
+            CHANGEME::from(u64::MAX as u128 * 2)
+        );
+    }
+
+    #[test]
+    fn test_large_add() {
+        assert_eq!(
+            CHANGEME::from(u128::MAX) + CHANGEME::from(u128::MAX),
+            CHANGEME::from(BigUint::from(u128::MAX) * BigUint::from(2_u32))
+        );
     }
 }
