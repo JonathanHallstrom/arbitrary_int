45e9f83328bbf9ecba6772bd45383289bdc08dc4
JonathanHallstrom <lmj.hallstrom@gmail.com>
2023-04-07 12:42:11 +0200

    minor changes to tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -4,7 +4,7 @@ mod tests {
     use num_bigint::BigUint;
 
     #[test]
-    fn test_large_conversion() {
+    fn large_conversion() {
         assert_eq!(
             CHANGEME::from(u128::MAX),
             CHANGEME::from(BigUint::from(u128::MAX))
@@ -16,7 +16,7 @@ mod tests {
     }
 
     #[test]
-    fn test_small_add() {
+    fn small_add() {
         for i in (0..u64::MAX).step_by(2_906_698_589_538_647) {
             for j in (0..u64::MAX).step_by(9_314_729_677_867_609) {
                 assert_eq!(
@@ -32,15 +32,15 @@ mod tests {
     }
 
     #[test]
-    fn test_medium_add() {
+    fn medium_add() {
         {
             // step by doesnt work for value > usize::MAX so I made my own for loops
             let mut i = 0;
-            while i < u128::MAX - 9355922656428016303878336798081429281 {
-                i += 9355922656428016303878336798081429281;
+            while i < u128::MAX - 9_355_922_656_428_016_303_878_336_798_081_429_281 {
+                i += 9_355_922_656_428_016_303_878_336_798_081_429_281;
                 let mut j = 0;
-                while j < u128::MAX - 3162064039811835189013542041112339911 {
-                    j += 3162064039811835189013542041112339911;
+                while j < u128::MAX - 3_162_064_039_811_835_189_013_542_041_112_339_911 {
+                    j += 3_162_064_039_811_835_189_013_542_041_112_339_911;
                     assert_eq!(
                         CHANGEME::from(i) + CHANGEME::from(j),
                         CHANGEME::from(BigUint::from(i) + BigUint::from(j))
@@ -59,7 +59,7 @@ mod tests {
     }
 
     #[test]
-    fn test_large_add() {
+    fn large_add() {
         assert_eq!(
             CHANGEME::from(u128::MAX) + CHANGEME::from(u128::MAX),
             CHANGEME::from(BigUint::from(u128::MAX) * BigUint::from(2_u32))
@@ -67,7 +67,7 @@ mod tests {
     }
 
     #[test]
-    fn test_small_mul() {
+    fn small_mul() {
         for i in (0..u64::MAX).step_by(2_906_698_589_538_647) {
             for j in (0..u64::MAX).step_by(9_314_729_677_867_609) {
                 assert_eq!(
@@ -82,15 +82,15 @@ mod tests {
     }
 
     #[test]
-    fn test_medium_mul() {
+    fn medium_mul() {
         {
             // step by doesnt work for value > usize::MAX so I made my own for loops
             let mut i = 0;
-            while i < u128::MAX - 9355922656428016303878336798081429281 {
-                i += 9355922656428016303878336798081429281;
+            while i < u128::MAX - 9_355_922_656_428_016_303_878_336_798_081_429_281 {
+                i += 9_355_922_656_428_016_303_878_336_798_081_429_281;
                 let mut j = 0;
-                while j < u128::MAX - 3162064039811835189013542041112339911 {
-                    j += 3162064039811835189013542041112339911;
+                while j < u128::MAX - 3_162_064_039_811_835_189_013_542_041_112_339_911 {
+                    j += 3_162_064_039_811_835_189_013_542_041_112_339_911;
                     assert_eq!(
                         CHANGEME::from(i) * CHANGEME::from(j),
                         CHANGEME::from(BigUint::from(i) * BigUint::from(j))
@@ -109,7 +109,7 @@ mod tests {
     }
 
     #[test]
-    fn test_large_mul() {
+    fn large_mul() {
         assert_eq!(
             CHANGEME::from(u128::MAX) * CHANGEME::from(u128::MAX),
             CHANGEME::from(BigUint::from(u128::MAX).pow(2))
